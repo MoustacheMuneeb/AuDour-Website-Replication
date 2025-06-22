890b47532bbcf4cf06074e7c3e21085bb2eeacc7
MoustacheMuneeb <muneebnasir1241@gmail.com>
2025-06-22 13:12:53 +0500

    awais

--- src/assets/Components/Share/Offerbar.jsx
+++ src/assets/Components/Share/Offerbar.jsx
@@ -4,7 +4,7 @@ const Offerbar = () => {
   return (
     <section className='container flex-wrap items-center flex justify-between bg-black bg-fixed'>
         <div className="text-white ps-40 pe-12 pt-3 pb-3  !text-center">
-            <p className='ps-105 h-[20px] w-[23px]'>
+            <p className='ps-105 h-[20px] '>
                 GET EXTRA 15% OFF ON ALL CREDIT/DEBIT CARDS
             </p>
         </div>
